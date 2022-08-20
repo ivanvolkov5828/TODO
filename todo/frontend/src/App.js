dbf9c6f9a7db854f389a240a472f4e803b9a0462
ivanvolkov <volkov.ivan20032012@yandex.ru>
2022-08-21 01:54:58 +0300

    №5

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+
 import UserList from './components/User.js'
-import Menu from "./components/Menu.js";
-import Footer from "./components/Footer.js";
+import ProjectList from './components/Project.js'
+import TodoList from './components/Todo.js'
+
+import Menu from './components/Menu.js';
+import Footer from './components/Footer.js';
 import axios from 'axios'
+import {HashRouter, Route, Routes, BrowserRouter} from 'react-router-dom'
+import NotFound404 from './components/NotFound';
+import ProjectFilterList from './components/OneProject.js'
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            'users': []
+            'users': [],
+            'projects': [],
+            'todos': []
         }
     }
 
@@ -24,21 +33,51 @@ class App extends React.Component {
                 }
             )
         }).catch(error => console.log(error))
+
+        axios.get("http://127.0.0.1:8000/api/projects/")
+        .then(response => {
+                const projects = response.data.results
+                this.setState(
+                {
+                    'projects': projects
+                }
+            )
+        }).catch(error => console.log(error))
+
+        axios.get("http://127.0.0.1:8000/api/todos/")
+        .then(response => {
+                const todos = response.data
+                this.setState(
+                {
+                    'todos': todos
+                }
+            )
+        }).catch(error => console.log(error))
+
     }
 
     render () {
         return (
-<div>
-    <div>
-        <Menu menu={this.state.menu}/>
-    </div>
-    <div>
-        <UserList users={this.state.users}/>
-    </div>
-    <div>
-        <Footer footer={this.state.footer}/>
-    </div>
-</div>
+
+            <div>
+                <BrowserRouter>
+
+                    <maincontent>
+
+                        <Routes>
+
+                            <Route path='/' exact element={ <UserList users={this.state.users}/> } />
+                            <Route path='/projects' exact element={ <ProjectList projects={this.state.projects}/> } />
+                            <Route path='/todos' exact element={ <TodoList todos={this.state.todos}/> } />
+                            <Route path='/project/:id' exact element={ <ProjectFilterList projects={this.state.projects}/> } />
+                            <Route path='*' exact element={<NotFound404/>}/>
+
+                        </Routes>
+
+                    </maincontent>
+
+                </BrowserRouter>
+            </div>
         )
     }
 }
---
+++ todo/frontend/src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound404 = ({location}) =>{
+
+    return(
+        <div>
+            <h2> Страница по адресу не найдена!</h2>
+        </div>
+    )
+}
+
+export default NotFound404

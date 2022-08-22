e8833bfdc00feb1687f928472720c6e1dca0bb46
ivanvolkov <volkov.ivan20032012@yandex.ru>
2022-08-22 20:17:44 +0300

    №6

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'project',
     'django_filters',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -149,5 +150,16 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
